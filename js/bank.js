551735a21cbd424875e38793bb977ffbe9007193
Forhad Ahmed <ahmedforhad5@gmail.com>
2022-11-24 15:45:44 +0600

    chart updated

--- js/bank.js
+++ js/bank.js
@@ -118,3 +118,33 @@ new Chart("barChart", {
 });
 
 // End barChart Area
+
+
+// Start lineChart Area
+
+var lineXValues = [100,200,300,400,500,600,700,800,900,1000];
+
+new Chart("lineMyChart", {
+  type: "line",
+  data: {
+    labels: lineXValues,
+    datasets: [{ 
+      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+      borderColor: "red",
+      fill: false
+    }, { 
+      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+      borderColor: "green",
+      fill: false
+    }, { 
+      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+      borderColor: "blue",
+      fill: false
+    }]
+  },
+  options: {
+    legend: {display: false}
+  }
+});
+
+// End lineChart Area
